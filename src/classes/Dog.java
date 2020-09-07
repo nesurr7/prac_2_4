8a4d6954117bef7dd46a946b01baebd6473d0a94
Nesurr <nesurr7@gmail.com>
2020-09-08 00:23:58 +0300

    Практика 2 , Задание 4 Реализация класса - Dog, и его дальнейшее использование в массиве собак в питомнике

---
+++ src/classes/Dog.java
@@ -0,0 +1,50 @@
+package classes;
+public class Dog {
+    //Свойства
+    String nameOfDog;
+    int ageOfDog;
+
+    //Конструкторы
+    Dog(String nameOfDog, int ageOfDog) {
+        this.nameOfDog = nameOfDog;
+        this.ageOfDog = ageOfDog;
+    }
+
+    Dog() {
+        this.nameOfDog = "undefined";
+        this.ageOfDog = -1;
+    }
+    //Геттеры
+
+    public int getAgeOfDog() {
+        return ageOfDog;
+    }
+
+    public String getNameOfDog() {
+        return nameOfDog;
+    }
+
+    //Сеттеры
+
+
+    public void setAgeOfDog(int ageOfDog) {
+        this.ageOfDog = ageOfDog;
+    }
+
+    public void setNameOfDog(String nameOfDog) {
+        this.nameOfDog = nameOfDog;
+    }
+
+    public int dogAgeToHuman() {
+        return ageOfDog * 7;
+    }
+
+    @Override
+    public String toString() {
+        return "_____________\n"
+                + "Имя собаки:" + nameOfDog + "\n"
+                + "Возраст собаки:" + ageOfDog + "\n"
+                + "Возраст собаки в возрасте людей:"
+                + dogAgeToHuman() + "\n_____________";
+    }
+}
